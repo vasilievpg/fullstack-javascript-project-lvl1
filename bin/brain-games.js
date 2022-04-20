c0218fab585a2b89a58b95ee566114f7e6357bb3
Pavel Vasilev <vasilievpg@gmail.com>
2022-04-20 13:20:52 +0300

    Шаг 3

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,3 +1,4 @@
 #!/usr/bin/env node
+import welcomeName from "../src/cli.js";
 
-console.log('Welcome to the Brain Games!');
+welcomeName();
---
+++ src/cli.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+import readlineSync from 'readline-sync';
+
+const welcomeName = () => {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+};
+
+export default welcomeName;
